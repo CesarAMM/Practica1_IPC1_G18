cbeca8c6937a8bc0444e7932eff276e746103015
Adolfo Mazariegos <csaradolfo139@outlook.com>
2021-06-02 21:31:13 -0600

    first commit

---
+++ src/practica_1_ipc_1/Practica_1_IPC_1.java
@@ -0,0 +1,16 @@
+/*
+    Cesar  Mazariegos
+    Cristopher Mazariegos 
+    Ana Ordóñez 
+    Oscar Muz 
+ */
+package practica_1_ipc_1;
+
+
+public class Practica_1_IPC_1 {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
