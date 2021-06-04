fd37e57ecfbd56e7a1137b3fcb06059801777bd0
Adolfo Mazariegos <csaradolfo139@outlook.com>
2021-06-03 20:33:42 -0600

    Almacenamiento de datos

--- src/practica_1_ipc_1/Funciones_Cesar.java
+++ src/practica_1_ipc_1/Funciones_Cesar.java
@@ -4,4 +4,7 @@ package practica_1_ipc_1;
 
 public class Funciones_Cesar {
     
+    public static void Mtriz_transpuesta(String matriz){
+        
+    }
 }
--- src/practica_1_ipc_1/Practica_1_IPC_1.java
+++ src/practica_1_ipc_1/Practica_1_IPC_1.java
@@ -12,11 +12,12 @@ import java.util.Scanner;
 
 
 public class Practica_1_IPC_1 {
-
     public static void main(String[] args) {
         int dato = 0;
         boolean estado = true;
+        String[][] matriz = new String[26][2];
         do{
+            boolean error = true;
             Scanner scanner;
             System.out.println(""
                         + "*******************************\n"
@@ -36,24 +37,33 @@ public class Practica_1_IPC_1 {
                 scanner = new Scanner(System.in);
                 System.out.print("--> Ingrese un digito: ");
                 dato = scanner.nextInt();
+            } catch (Exception e) {
+                error= false;
+                System.out.println("\n     |--------------------------------------|");
+                System.out.println("     |Error: " +e);
+                System.out.println("     |Lo que ha ingresado no es un numero.  |");
+                System.out.println("     |--------------------------------------|\n");
+            }
+            if(error){
                 switch(dato){
                     case 1:
                         String ruta = "";
                         do{
-                            scanner = new Scanner(System.in);
-                            System.out.print("----> Ingrese la ruta del archivo: ");
-                            ruta = scanner.nextLine();
+                        scanner = new Scanner(System.in);
+                        System.out.print("----> Ingrese la ruta del archivo: ");
+                        ruta = scanner.nextLine();
                             if(ruta.length() == 0){
                                 System.out.println(""
                                         + "\n     |-----------------------|"
                                         + "\n     | No has ingresado nada |"
                                         + "\n     |-----------------------|");
                             }else{
-                                LeerArchivo(ruta);
+                                matriz = LeerArchivo(ruta);
                             }
                         }while(ruta.length() == 0);
                         break;
                     case 2:
+                        Ver_Matrices(matriz);
                         break;
                     case 3:
                         break;
@@ -77,24 +87,73 @@ public class Practica_1_IPC_1 {
                         System.out.println("Has ingresado: " + dato + ", Es un rango incorrecto.");
                         break;
                 }
-            } catch (Exception e) {
-                System.out.println("\n     |--------------------------------------|");
-                System.out.println("     |Error: " +e);
-                System.out.println("     |Lo que ha ingresado no es un numero.  |");
-                System.out.println("     |--------------------------------------|\n");
             }
         }while(estado);
     }
     
-    public static void LeerArchivo(String ruta){
+    public static String[][] LeerArchivo(String ruta){
+        String Cadena="";
+        int Filas =0;
         try {
             Scanner input = new Scanner(new File(ruta));
             while (input.hasNextLine()){
-                String line = input.nextLine();
-                System.out.println(line);
+                Filas++;
+                Cadena = Cadena + input.nextLine() +"\n";
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+        String Matrices[][] = new String[Filas][2];
+        for (int i = 0; i < Filas; i++) {
+            String nombre = Cadena.split("\n")[i].split(":")[0];//Nombre de la Matriz
+            String matrizAux = Cadena.split("\n")[i].split(":")[1].replace(" ", "");//La matriz
+            String[] auxFila = matrizAux.split(";");//Obtego las filas por la separacion de ;
+            int contador =0;
+            int colum = auxFila[0].split(",").length;
+            for (int j = 0; j < auxFila.length; j++) {
+                int columAux = auxFila[j].split(",").length;
+                if(colum == columAux){
+                    contador++;
+                }
+            }
+            if(contador == auxFila.length){
+                for (int j = 0; j < 26; j++) {
+                    if(Matrices[j][0] != null){
+                        if(Matrices[j][0].equals(nombre)){
+                            Matrices[j][1] = matrizAux;
+                            break;
+                        }
+                    }else{
+                        Matrices[j][0] = nombre;
+                        Matrices[j][1] = matrizAux;
+                        break;
+                    }
+                }
+            }else{
+                System.out.println("La matriz: "+ nombre +". No pudo se ingresada");
+            }
+        }        
+        return Matrices;
+    }
+    
+    public static void Ver_Matrices(String[][] matriz){
+        for(int i = 0; i < matriz.length; i++){
+            if(matriz[i][0] != null){
+                String pintar = "", cadena= "";
+                cadena = matriz[i][1];
+                String[] auxFilas = cadena.split(";");
+                pintar = matriz[i][0] + "\n";
+                for (int j = 0; j < auxFilas.length; j++) {
+                    String[] auxColumnas = auxFilas[j].split(",");
+                    for (int k = 0; k < auxColumnas.length; k++) {
+                        pintar += "|"+auxColumnas[k]+"| ";
+                    }
+                    pintar += "\n";
+                }
+                System.out.println("Matriz: " + pintar);
+            }
+        }
     }
 }
+
+//C:\Users\cesar.monroy\Desktop\ipc\Prueba.txt
